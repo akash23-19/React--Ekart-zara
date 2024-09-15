c93890c2c3e59c17a12b82ae08088a87faa7ee7f
Akash Singh <akashsingh@Akashs-MacBook-Air.local>
2024-09-15 23:23:58 +0530

    first commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          Welcome to Zara, your ultimate destination for stylish men's and
+          women's clothing and jewelry! At Zara, we pride ourselves on offering
+          a diverse range of high-quality fashion pieces that cater to all
+          tastes and occasions. Whether you're looking for trendy casual wear,
+          elegant evening outfits, or the perfect piece of jewelry to complement
+          your look, we have something for everyone. Our mission is to bring you
+          the best in fashion, combining affordability with style. We carefully
+          select each item in our collection to ensure that our customers enjoy
+          not only the latest trends but also timeless classics. Zara is
+          committed to providing a seamless shopping experience, with easy
+          navigation, excellent customer service, and fast shipping, so you can
+          shop with confidence. Explore our collections today and find the
+          perfect additions to your wardrobe or jewelry box. At Zara, we believe
+          fashion is about expressing your unique style, and we’re here to help
+          you do just that!
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img
+                className="card-img-top img-fluid"
+                src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                height={160}
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img
+                className="card-img-top img-fluid"
+                src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                height={160}
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img
+                className="card-img-top img-fluid"
+                src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                height={160}
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img
+                className="card-img-top img-fluid"
+                src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                height={160}
+              />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
